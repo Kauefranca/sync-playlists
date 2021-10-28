dd1f3d8bc77cce01aa51f39264a02d97ed6d118e
Kaue França <kauefranca04@gmail.com>
2021-10-28 16:38:29 -0300

    :sparkles: 1.1 - Youtube/Spotify full compability

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 A simple project for sync playlists from multiple plataforms.
 
 ## Todo
-- [ ] Youtube
+- [x] Youtube
     - [x] Get every video from given playlist.
-    - [ ] Verify if video type is music, if not ignore.
-    - [ ] Save all music video title in a array.
-- [ ] Spotify
-    - [ ] Search for musics.
-    - [ ] Create playlists and add musics to it.
+    - [x] Verify if video type is music, if not ignore.
+    - [x] Save all music video title in a array.
+- [x] Spotify
+    - [x] Search for musics.
+    - [x] Create playlists and add musics to it.
+- [ ] Make a simple site with express to get the user acces token and sync playlists
--- index.js
+++ index.js
@@ -1,66 +1,24 @@
-const axios = require('axios');
-const fs = require('fs');
+const youtube = require('./lib/youtube');
+const spotify = require('./lib/spotify');
+const { godlessRegex } = require('./lib/utils');
 
-const youtubeApiUrl = 'https://www.youtube.com/youtubei/v1/';
-const playlistUrl = 'https://www.youtube.com/playlist?list=PLPRsRurBd7FRrRwpp4tEYMUlYY2kOzMg5';
+const token = '';
 
 ;(async() => {
-    var allTitles;
-    var request = await axios({
-        method: 'get',
-        url: playlistUrl,
-        headers: {
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36,gzip(gfe)',
-        }
-    })
-    .then((res) => res.data)
-    .catch((err) => err);
+    var userInfo = await spotify.getUserInfo(token)
+    if (!userInfo) return console.log('Invalid access token!');
 
-    var playlist = JSON.parse(request.split('ytInitialData =')[1].split(';')[0]);
-    var videoList = playlist.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents;
+    var videoList = await youtube.getVideoList('https://www.youtube.com/playlist?list=PLPRsRurBd7FRrRwpp4tEYMUlYY2kOzMg5');
+    var playlistId = await spotify.createPlaylist(token, userInfo.display_name, { name: 'playlist-sync', description: 'Playlist converted from Youtube' })
+    if (playlistId.error) return console.log('An error occurred creating the playlist.');
 
-    if (videoList.length == 101) {
-        var browseApiKey = request.split('INNERTUBE_API_KEY":"')[1].split('"')[0];
-        var cfg = removeUnicode('{"CLIENT_CANARY_STATE' + request.split('CLIENT_CANARY_STATE')[1].split(');')[0]);
-        cfg = JSON.parse(cfg);
-
-        var continuation = videoList.pop()
-        var continuationToken = continuation.continuationItemRenderer.continuationEndpoint.continuationCommand.token;
-
-        var data = {
-            context: {
-                client: cfg.INNERTUBE_CONTEXT.client,
-                user: cfg.INNERTUBE_CONTEXT.user,
-                request: cfg.INNERTUBE_CONTEXT.request,
-                clickTracking: cfg.INNERTUBE_CONTEXT.clickTracking
-            },
-            continuation: continuationToken
-        }
-
-        var loadVideos = await axios({
-            method: 'post',
-            url: `${youtubeApiUrl}browse?key=${browseApiKey}`,
-            data
-        })
-        .then((res) => res.data)
-        .catch((err) => err);
-
-        for (let i of (loadVideos.onResponseReceivedActions[0].appendContinuationItemsAction.continuationItems)) {
-            videoList.push(i);
+    for (let i in videoList) {
+        var title = videoList[i].title.replace(godlessRegex, ' ').replace(/([ ]{2,})/gi, ' ').trim();
+        if (title) {
+            var uri = await spotify.queryTrackUri(token, encodeURIComponent(title));
+            if (!uri.error) {
+                await spotify.addTrackOnPlaylist(token, playlistId.data, uri.data);
+            }
         }
     }
-    var titles = videoList.map(function (obj) {
-        if (!obj.playlistVideoRenderer) return;
-        return obj.playlistVideoRenderer.title.runs[0].text;
-    });
-
-    console.log(titles);
 })();
-
-function removeUnicode(str) {
-    var r = /\\u([\d\w]{4})/gi;
-    str = str.replace(r, function (match, grp) {
-        return String.fromCharCode(parseInt(grp, 16));
-    });
-    return decodeURIComponent(str);
-}

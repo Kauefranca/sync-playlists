c402a0217cc04d14ae9bf25b21bd190c741607b5
Kaue França <kauefranca04@gmail.com>
2021-10-29 12:04:02 -0300

    :sparkles: Release 1.2

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
 # Sync Playlists
 
-A simple project for sync playlists from multiple plataforms.
+A simple project that converts playlists from Youtube to Spotify.
+
+# Requirements
+> nodejs & npm
+
+## Instalation
+
+```bash
+> git clone https://github.com/Kauefranca/sync-playlists
+> cd sync-playlists
+> npm install
+```
+
+## How to use
+
+This project uses SpotifyOAuth, see [Spotify API documentation](https://developer.spotify.com/documentation/general/guides/authorization/)
+
+Create a file called .env that should look like this:
+
+    client_id=xxxxxxxxxxx
+    client_secret=xxxxxxxxxxx
+    redirect_uri=http://localhost:8888/callback
+
+For this credentials create a account on [Spotify for Developers](https://developer.spotify.com/) and make sure to add the redirect_url on Dashboard > Edit Settings > Redirect URIs
+
+
+Now with everything set just run:
+
+```bash
+> node index.js
+```
+
+If you find some issue fell free to open a [issue](https://github.com/Kauefranca/sync-playlists/issues) or a [pull request](https://github.com/Kauefranca/sync-playlists/pulls)
+
 
 ## Todo
-- [x] Youtube
-    - [x] Get every video from given playlist.
-    - [x] Verify if video type is music, if not ignore.
-    - [x] Save all music video title in a array.
-- [x] Spotify
-    - [x] Search for musics.
-    - [x] Create playlists and add musics to it.
-- [ ] Make a simple site with express to get the user acces token and sync playlists
+- [x] Full Youtube compability
+- [x] Full Spotify compability
+
+## License
+
+[MIT](https://github.com/Kauefranca/sync-playlists/blob/master/LICENSE)
+
+Made with 💜 by Kauê França
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+const readLine = require('readline-sync');
 const youtube = require('./lib/youtube');
 const spotify = require('./lib/spotify');
 const { godlessRegex } = require('./lib/utils');
-
-const token = '';
+const { getUserConsent } = require('./lib/getUserConsent');
 
 ;(async() => {
-    var userInfo = await spotify.getUserInfo(token)
-    if (!userInfo) return console.log('Invalid access token!');
+    const token = await getUserConsent();
+    var userInfo = await spotify.getUserInfo(token.access_token);
+    if (!userInfo) return console.log('An error ocurred during the Authentication');
 
-    var videoList = await youtube.getVideoList('https://www.youtube.com/playlist?list=PLPRsRurBd7FRrRwpp4tEYMUlYY2kOzMg5');
-    var playlistId = await spotify.createPlaylist(token, userInfo.display_name, { name: 'playlist-sync', description: 'Playlist converted from Youtube' })
-    if (playlistId.error) return console.log('An error occurred creating the playlist.');
+    var videoList = await youtube.getVideoList(readLine.question('Insert the Youtube playlist URL: '));
+    var playlistId = await spotify.createPlaylist(token.access_token, userInfo.display_name, { name: readLine.question('Insert a name for the Spotify playlist: '), description: 'Playlist auto converted from Youtube using Kauefranca/sync-playlists' })
+    if (playlistId.error) return console.log(playlistId.error);
+    console.log('Converting playlist, please wait...');
 
     for (let i in videoList) {
         var title = videoList[i].title.replace(godlessRegex, ' ').replace(/([ ]{2,})/gi, ' ').trim();
         if (title) {
-            var uri = await spotify.queryTrackUri(token, encodeURIComponent(title));
+            var uri = await spotify.queryTrackUri(token.access_token, encodeURIComponent(title));
             if (!uri.error) {
-                await spotify.addTrackOnPlaylist(token, playlistId.data, uri.data);
-            }
+                await spotify.addTrackOnPlaylist(token.access_token, playlistId.data, uri.data);
+            } else console.log(decodeURIComponent(uri.error.message));
         }
     }
 })();
